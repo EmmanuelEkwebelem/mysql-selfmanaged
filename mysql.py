72cf939b953bf3ff75d0404f757cc15bf0be63bb
EmmanuelEkwebelem <93545665+EmmanuelEkwebelem@users.noreply.github.com>
2022-12-15 23:43:29 -0500

    Update

--- mysql.py
+++ mysql.py
@@ -4,15 +4,21 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-mysql_host = os.getenv('HOST')
-mysql_user = os.getenv('USER')
-mysql_password = os.getenv('KEY')
-mysql_data = os.getenv('DATABASE')
+
+# mysql_host = '4.236.187.210'
+# mysql_user = 'newuser'
+# mysql_password = 'password'
+# mysql_data = 'db1'
+
+mysql_host = os.getenv("MYSQL_HOSTNAME")
+mysql_user = os.getenv('MYSQL_USERNAME')
+mysql_password = os.getenv('MYSQL_PASSWORD')
+mysql_data = os.getenv('MYSQL_DATABASE')
 
 db = create_engine(f'mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_data}')
 
 query = 'select * from db1.UEFA;'
-query 
+query
 
 df = pd.read_sql(query, con = db)
 df
